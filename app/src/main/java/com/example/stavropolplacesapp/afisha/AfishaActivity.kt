b44733d822b48dd9ee6e5806a2e049594cd932e2
chernyaev <Ilya.chernyaev@gorparkovki.ru>
2024-10-21 12:25:37 +0300

    Добавил тулбар "афиши"

--- app/src/main/java/com/example/stavropolplacesapp/afisha/AfishaActivity.kt
+++ app/src/main/java/com/example/stavropolplacesapp/afisha/AfishaActivity.kt
@@ -1,10 +1,14 @@
 package com.example.stavropolplacesapp.afisha
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.example.stavropolplacesapp.R
 
 class AfishaActivity : AppCompatActivity() {
@@ -12,6 +16,31 @@ class AfishaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_afisha)
+
+        // Инициализация Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        // Добавляем кнопку назад
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.ic_arrow_back)
+        toolbar.navigationIcon?.setTint(ContextCompat.getColor(this, R.color.black)) // Устанавливаем чёрный цвет
+        // Устанавливаем заголовок в центр
+        supportActionBar?.title = ""
+        val titleTextView: TextView = findViewById(R.id.toolbar_title)
+        titleTextView.text = "Афиша"
+        // Добавляем действие при нажатии на кнопку возврата
+        toolbar.setNavigationOnClickListener {
+            onBackPressed() // Возврат на предыдущий экран
+        }
+
+
+        // Прозрачный статус-бар с видимыми иконками
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT
+
+        // Используем светлый статус-бар для видимых иконок (черные иконки)
+        window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
         // Находим WebView и контейнер с изображением
         val webView: WebView = findViewById(R.id.afishaWebView)
